6ed2c5194e3091b0f9ed9b6614889c7d68b01b2a
phuennae <liverpoolnae@gmail.com>
2023-07-26 16:22:37 +0700

    completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,30 @@
 "use client";
 
+import { UserCard } from "@/components/UserCard";
+import { cleanUser } from "@/libs/cleanUser";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function RandomUserPage() {
   //user = null or array of object
   const [users, setUsers] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [genAmount, setGenAmount] = useState(1);
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
+
+  useEffect(() => {
+    if (isFirstLoad) {
+      setIsFirstLoad(false);
+      return;
+    }
+    const strAmount = JSON.stringify(genAmount);
+    localStorage.setItem("genAmount", strAmount);
+  }, [genAmount]);
+
+  useEffect(() => {
+    const strAmount = localStorage.getItem("genAmount");
+    setGenAmount(JSON.parse(strAmount));
+  }, []);
 
   const generateBtnOnClick = async () => {
     setIsLoading(true);
@@ -17,6 +34,8 @@ export default function RandomUserPage() {
     setIsLoading(false);
     const users = resp.data.results;
     //Your code here
+    const cleanedUsers = users.map((user) => cleanUser(user));
+    setUsers(cleanedUsers);
     //Process result from api response with map function. Tips use function from /src/libs/cleanUser
     //Then update state with function : setUsers(...)
   };
@@ -40,7 +59,17 @@ export default function RandomUserPage() {
       {isLoading && (
         <p className="display-6 text-center fst-italic my-4">Loading ...</p>
       )}
-      {users && !isLoading && users.map(/*code map rendering UserCard here */)}
+      {users &&
+        !isLoading &&
+        users.map((user) => (
+          <UserCard
+            key={user.email}
+            name={user.name}
+            imgUrl={user.imgUrl}
+            address={user.address}
+            email={user.email}
+          />
+        ))}
     </div>
   );
 }
